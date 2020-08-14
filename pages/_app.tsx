c1d595ca3df6bbe5bbe2a33b97900223140ba6af
Matt Thomas <mattcthomas@me.com>
2020-08-15 00:12:31 +0100

    chore: update linter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from 'next/app'
-import { FC } from 'react'
 import Head from 'next/head'
-import { Layout } from 'components/layout'
+import { FC } from 'react'
+
+import { Layout } from '../components/layout'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -12,75 +13,77 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      <style jsx global>{`
-        :root {
-          --bg-color: #fff;
-          --primary-color: orange;
-          --secondary-color: pink;
-          --font-color: #232323;
-        }
+      <style global={true} jsx={true}>
+        {`
+          :root {
+            --bg-color: #fff;
+            --primary-color: orange;
+            --secondary-color: pink;
+            --font-color: #232323;
+          }
 
-        * {
-          padding: 0;
-          margin: 0;
-          box-sizing: border-box;
-        }
+          * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+          }
 
-        body {
-          background: var(--bg-color);
-          color: var(--font-color);
-          font-size: 16px;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
-            Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-          font-weight: 400;
-        }
+          body {
+            background: var(--bg-color);
+            color: var(--font-color);
+            font-size: 16px;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+              Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+            font-weight: 400;
+          }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          line-height: 9rem;
-          font-weight: 600;
-        }
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            line-height: 9rem;
+            font-weight: 600;
+          }
 
-        h2,
-        h3,
-        h4 {
-          line-height: 6rem;
-        }
+          h2,
+          h3,
+          h4 {
+            line-height: 6rem;
+          }
 
-        h5,
-        h6 {
-          line-height: 3rem;
-        }
+          h5,
+          h6 {
+            line-height: 3rem;
+          }
 
-        h1 {
-          font-size: 6.10352em;
-        }
+          h1 {
+            font-size: 6.10352em;
+          }
 
-        h2 {
-          font-size: 4.88281em;
-        }
+          h2 {
+            font-size: 4.88281em;
+          }
 
-        h3 {
-          font-size: 3.90625em;
-        }
+          h3 {
+            font-size: 3.90625em;
+          }
 
-        h4 {
-          font-size: 3.125em;
-        }
+          h4 {
+            font-size: 3.125em;
+          }
 
-        h5 {
-          font-size: 2.5em;
-        }
+          h5 {
+            font-size: 2.5em;
+          }
 
-        h6 {
-          font-size: 2em;
-        }
-      `}</style>
+          h6 {
+            font-size: 2em;
+          }
+        `}
+      </style>
     </>
   )
 }
