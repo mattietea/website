e8b7db22911d7667db1b7af1bc6e5fc53a0571c2
Matthew Thomas <mattcthomas@me.com>
2020-11-23 18:00:19 +0000

    style: eslint --fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-})
+});
 
 module.exports = withMDX({
   pageExtensions: ['md', 'mdx', 'tsx', 'ts'],
-})
+});
