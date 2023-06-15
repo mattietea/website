490dd84ba2af258dac820bd6df518d46bb0a350d
mattietea <mattcthomas@me.com>
2023-06-15 22:50:13 +0100

    update

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  experimental: {
+    assets: true,
+  },
+});
